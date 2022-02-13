64aed458319c0730802d540624cb2e3504e66d1b
Gabriel <gabeklav@gmail.com>
2022-02-12 23:51:06 -0500

    Prod build!

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ const stylesHandler = "style-loader";
 
 const config = {
 	entry: "./src/index.ts",
-	watch: true,
+	// watch: true,
 	output: {
 		path: path.resolve(__dirname, "docs"),
 		clean: true,
